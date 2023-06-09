d23fad7575cc0e1295f7e8bb5c802b2c77b785c0
LeeSeungYeon <masakisqq@naver.com>
2023-06-09 12:00:22 +0900

    프로젝트 저장소 초기화

---
+++ src/test/java/yogurrr/hello/jenkinscicd/JenkinsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package yogurrr.hello.jenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
